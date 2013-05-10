fd99ef6bd78d33b2267ed925822dc51ed2eac65c
Victor Lowther <victor.lowther@gmail.com>
2013-05-10 08:57:55 -0500

    Add support for fetching from remotes.

--- git/remote.go
+++ git/remote.go
@@ -6,8 +6,12 @@ import (
 	"errors"
 )
 
-func (r *Repo) Remotes() map[string]string {
-	res := make(map[string]string)
+// Type to hold our map of remote names -> remote specifiers.
+type RemoteMap map[string]string
+
+// Get our list of remotes by parsing the git config.
+func (r *Repo) Remotes() RemoteMap {
+	res := make(RemoteMap)
 	conf,err := r.Config()
 	if err != nil {
 		return res
@@ -21,6 +25,7 @@ func (r *Repo) Remotes() map[string]string {
 	return res
 }
 
+// Add a new remote.
 func (r *Repo) AddRemote(name,url string) (err error){
 	remotes := r.Remotes()
 	if remotes[name] != "" {
@@ -34,6 +39,7 @@ func (r *Repo) AddRemote(name,url string) (err error){
 	return nil
 }
 
+// Destroy an old remote mapping
 func (r *Repo) ZapRemote(name string) (err error){
 	remotes := r.Remotes()
 	if remotes[name] == "" {
@@ -45,4 +51,63 @@ func (r *Repo) ZapRemote(name string) (err error){
 		return err
 	}
 	return nil
+}
+
+// Helper type for holding the status of a fetch from a single remote.
+type FetchStatus struct {
+	Ok bool
+	Remote string
+}
+
+// Fetch updates from a single remote.
+func (r *Repo) fetchOne(remote string, ok chan FetchStatus) {
+	cmd, _, _ := r.Git("fetch","-q","-t",remote)
+	err := cmd.Run()
+	ok <- FetchStatus{
+		Ok: (err == nil),
+		Remote: remote,
+	}
+	return
+}
+
+// Helper to enable empty slice -> all remotes the repo knows about.
+func (r *Repo) allRemotes(remotes []string) ([]string){
+	if len(remotes) > 0 {
+		return remotes
+	}
+	for k,_ := range r.Remotes() {
+		remotes = append(remotes,k)
+	}
+	return remotes
+}
+
+// Fetch updates from the passed remotes.
+// This expects to be called as a goroutine.
+func (r *Repo) AsyncFetch(remotes []string, ok chan FetchStatus) {
+	remotes = r.allRemotes(remotes)
+	for _,v := range remotes {
+		go r.fetchOne(v,ok)
+	}
+}
+
+// Type that holds our map of remote names -> whether we fetched all updates from the remote.
+type FetchMap map[string]bool
+
+// Fetch all updates from our remotes in parallel.
+func (r *Repo) Fetch(remotes []string) (res bool, items FetchMap) {
+	ok := make(chan FetchStatus)
+	items = make(FetchMap)
+	res = true
+	remotes = r.allRemotes(remotes)
+	go r.AsyncFetch(remotes,ok)
+	for {
+		token := <- ok
+		items[token.Remote]=token.Ok
+		res = res && token.Ok
+		if len(items) == len(remotes) {
+			break
+		}
+	}
+	close(ok)
+	return res,items
 }
